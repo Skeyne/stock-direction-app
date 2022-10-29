e08ad6a7eab60f37a97f97ae9e5a76c08e957ad9
Skeyne <andremrodrigues99@gmail.com>
2022-10-29 13:07:51 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
+import yfinance as yf
+import mplfinance as mpf
+from data_preprocessing import TimeSeriesDataset
+from sklearn.svm import SVR, SVC
+from sklearn.preprocessing import StandardScaler
+import os
+from sklearn import metrics
+
+st.set_page_config(
+    page_title="App?? FR??",
+    page_icon=":sweat_drops:"   
+)
+
+with st.sidebar:
+    st.title("Sidebar")
+    st.radio("Navigation",["Visualise", "Train","About"])
+    st.sidebar.success("We did it reddit!")
+
+if 'started_analyis' not in st.session_state:
+    st.session_state['plot_button'] = False
+    st.session_state['ticker_button'] = False
+    st.session_state['started_analysis'] = True
+
+st.title("Stock direction prediction")
+st.write("Select ticker")
+symbol = st.selectbox("SYMBOL:",('AAPL','AMZN','MSFT','GOOGL','TSLA','^GSPC','^DJI'))
+ticker_cached = os.path.exists(symbol+'.pkl')
+period = 'max'
+interval = '1d'
+if st.button("Reload"):
+    st.session_state['ticker_button'] = True
+    ticker_data = yf.download(tickers=symbol, period=period, interval=interval, prepost=True, rounding=False)
+    ticker_data.to_pickle(symbol+'.pkl')
+if ticker_cached:
+    ticker_data = pd.read_pickle(symbol+'.pkl')
+    sample_n = st.number_input("Intervals to extract:",min_value = 0)
+    data = TimeSeriesDataset(ticker_data).sample(sample_n)
+    c1, c2 = st.columns([1, 1])
+    c1.write("Raw ticker data: "+str(ticker_data.shape))
+    c1.dataframe(ticker_data.head(5))
+    c2.write("Processed train set: "+str(data.train.shape))
+    c2.dataframe(pd.DataFrame(data.train).head(5))
+    ## PLOT CANDLES##
+    mc = mpf.make_marketcolors(up='g',down='r')
+    s  = mpf.make_mpf_style(marketcolors=mc)
+    fig, ax = mpf.plot(ticker_data,type='line',style=s,show_nontrading=False, figscale = 0.6,figratio = (12,4),returnfig=True)
+    st.pyplot(fig, use_container_width=True)
+    ##HYPERPARAMETERS
+    hp_c = st.number_input("C:",min_value = 0.0,value = 1.0)
+    ##SETUP, TRAIN, EVALUATE##
+    scaler = StandardScaler()   
+    scaler.fit(data.train)
+    c_weights = 1/(data.occurences/np.sum(data.occurences))
+    c_weights = c_weights/c_weights.min()
+    weight_dict = {i-1:c_weights[i] for i in range(len(c_weights)) }
+    classifier = SVC(kernel = 'rbf',C=hp_c,class_weight=weight_dict)
+    model = classifier.fit(scaler.transform(data.train), data.y_train)
+    score_train = model.score(scaler.transform(data.train), data.y_train)
+    score_val = model.score(scaler.transform(data.val), data.y_val)
+    st.write("R2 Train:",score_train,"R2 Val:",score_val)
+    prediction = model.predict(scaler.transform(data.val))
+    alt_accuracy = np.array([1 if (prediction[i] == data.y_val[i]) or (prediction[i] == data.y_val_pre[i]) else 0 for i in range(len(prediction))])
+    class_acc = metrics.accuracy_score(data.y_val, prediction, normalize=True, sample_weight=None)
+    st.write("Classification accuracy:","{:.2f}%".format(100*class_acc))
+    st.info("Classification accuracy is the percentage of correctly matching target labels guessed",icon="ℹ️")
+    effec_acc = np.sum(alt_accuracy)/len(alt_accuracy)
+    st.write("Effective accuracy: {:.2f}%".format(100*effec_acc))
+    st.info("Effective accuracy counts cases where the classifier wrongfully guessed a directon when the magnitude was too low but the actual direction matched", icon="ℹ️")
+    error = prediction != data.y_val
---
+++ data_preprocessing.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd 
+from stockstats import wrap
+
+#OHLC(AC)V
+class TimeSeriesDataset:
+    def __init__(self,data_raw):
+        self.data_raw = data_raw
+        self.process_data()
+
+    def sample(self,n_samples):
+        self.process_data(n_samples=n_samples)
+        return self
+
+    def process_data(self,n_samples: int=0):
+        window_size = 5
+        data = self.data_raw.iloc[:,[0,1,2,3]].copy()
+        
+        #data_indicators = self.data_raw.iloc[:,[0,1,2,3]].copy()
+        stockdf = wrap(self.data_raw.iloc[:,[0,1,2,3]])
+        data['21sma'] = stockdf['close_21_sma']
+        data['50sma'] = stockdf['close_50_sma']
+        data['100sma'] = stockdf['close_100_sma']
+        data['200sma'] = stockdf['close_200_sma']
+        data['rsi'] = stockdf['rsi'] * self.data_raw['Open']
+        data['macd'] = stockdf['macd']
+        data['macds'] = stockdf['macds']
+
+        data = data.to_numpy()
+        data = data[200:]
+        # shorten dataset
+        if n_samples != 0:
+            data = data[-n_samples:]
+
+        data_norm = np.zeros(data.shape)
+        for i in range(len(data)):
+            data_norm[i] = data[i,:]/data[i,0]
+        data_sliced = np.zeros((data_norm.shape[0]-window_size,window_size,data_norm.shape[1]))
+        for i in range(len(data_norm)-window_size):
+            data_sliced[i] = data_norm[i:i+window_size,:]
+        # extract specific columns
+        # columns_to_extract = range(1,12)
+        data_sliced = data_sliced[:,:,1:]
+        targets = data_norm[window_size:,3]
+        test_index = -4
+        print(targets[test_index],"i.e.",targets[test_index]-1,"return")
+        print(np.abs(targets-1)[test_index]>=0.007)
+        targets = np.where(np.abs(targets-1)>=0.01,np.sign(targets-1),0)
+        targets_pre = np.sign(targets-1)
+        self.targets_pre = targets_pre
+        occurences = np.bincount((targets+1).astype(int))
+        self.occurences = occurences
+        print(targets[test_index])
+        split = 0.90
+        split_index = int(len(data_sliced)*split)
+        self.y_train, self.y_val = np.split(targets,indices_or_sections=[split_index])
+        self.y_train_pre, self.y_val_pre = np.split(targets_pre,indices_or_sections=[split_index])
+        data_train, data_val = np.split(data_sliced,indices_or_sections=[split_index])
+        self.train = data_train.reshape((data_train.shape[0],data_train.shape[1]*data_train.shape[2]))
+        self.val = data_val.reshape((data_val.shape[0],data_val.shape[1]*data_val.shape[2]))
+        
+        print("Index to split at:",split_index)
+        print("Train data shape:",data_train.shape)
+        print("Train target shape:",self.y_train.shape)
+        print("Validation data shape:",data_val.shape)
+        print("Validation target shape:",self.y_val.shape)
